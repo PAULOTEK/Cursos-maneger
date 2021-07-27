220cffce3f5eb9b930932a315069a896ed84e83c
PAULOTEK <pauloalexandre820@gmail.com>
2021-07-26 22:55:35 -0300

    Atualização 26/07/2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
+import { CourseModules } from './model/course.module';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+;
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// importei CurselistCompenent no modulo, sempre fazer isso quando criar component ou pagina
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    CourseModules,
+    RouterModule.forRoot([
+
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
